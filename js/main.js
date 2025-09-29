7a2e025dd0ea5d400dc635c68df5667486472012
Hentokj <hent.aiokj@gmail.com>
2025-09-29 21:45:59 +0300

    style: add styles for directions section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-// var mixer = mixitup('.directions__list');
+$(function () {
+
+    var mixer = mixitup('.directions__list');
+
+    $('.directions__filter-btn').on('click', function () {
+        $('.directions__filter-btn').removeClass('directions__filter-btn__active');
+        $(this).addClass('directions__filter-btn__active');
+    })
+
+})

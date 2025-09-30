7ac83a3bca44a344c69306a8596786f780401675
Hentokj <hent.aiokj@gmail.com>
2025-10-01 00:49:46 +0300

    style: add styles for team section

--- js/main.js
+++ js/main.js
@@ -7,12 +7,24 @@ $(function () {
         $(this).addClass('directions__filter-btn__active');
     })
 
-    // $('.team__slider').slick({
-        
-    // })
-})
+    $('.team__slider').slick({
+        arrows: false,
+        slidesToShow: 4,
+        infinite: true,
+        draggable: false,
+        appendArrows: $('.team__slider-arrows'),
+        waitForAnimate: false,
+    })
 
-$(document).ready(function(){
-  $('.team__slider').slick({
-  });
-});
+    $('.team__slider-prev').on('click', function (e) {
+        e.preventDefault()
+
+        $('.team__slider').slick('slickPrev')
+    })
+
+    $('.team__slider-next').on('click', function (e) {
+        e.preventDefault()
+
+        $('.team__slider').slick('slickNext')
+    })
+})
